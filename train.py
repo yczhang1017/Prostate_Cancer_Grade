0f8ee3c6626e418b12ed938312224944478ac4b1
yczhang1017 <zhangyuchen12344@gmail.com>
2020-04-29 22:32:21 -0400

    Update train.py

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ parser.add_argument('--batch_size', default=16, type=int,
                     help='Batch size for training')
 parser.add_argument('-w','--workers', default=4, type=int,
                     help='Number of workers used in dataloading')
-parser.add_argument('--lr', default=0.01, type=float,
+parser.add_argument('--lr', default=0.001, type=float,
                     help='initial learning rate')
 parser.add_argument('-e','--epochs', default=15, type=int,
                     help='number of epochs to train')
