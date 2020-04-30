3d60578a140f814ccc3a0979faec05aead0703d3
yczhang1017 <zhangyuchen12344@gmail.com>
2020-04-29 22:25:15 -0400

    Update train.py

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ parser.add_argument('-ls','--log_step', default=5, type=int,
                     help='number of steps to print log')
 parser.add_argument('--step', default=5, type=int,
                     help='step to reduce lr')
-parser.add_argument('-a','--arch', default='efficientnet-b5', type=str,
+parser.add_argument('-a','--arch', default='efficientnet-b4', type=str,
                     help='architecture of EfficientNet')
 
 args = parser.parse_args()
