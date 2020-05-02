5136c60aaae769293d35ef1c42cda051d2c2d26a
yczhang1017 <zhangyuchen12344@gmail.com>
2020-05-02 02:28:14 -0400

    focalloss

--- segmentation.py
+++ segmentation.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import torch
 from torch import nn
+import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import pandas as pd
 import numpy as np
@@ -124,7 +125,21 @@ class ProstateSeg(Dataset):
         target = torch.from_numpy(np.array(mm).astype('int64'))
         return im,target
  
+ 
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=None, gamma=4, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduce = reduce
 
+    def forward(self, x, y):
+        s = F.log_softmax(x,1)
+        nll = F.nll_loss(s, y, self.alpha, reduction='none')
+        print(nll)
+        return ((1-s)**self.gamma *nll).mean()
+        
+    
     
 def adjust_lr(optimizer, epoch, args):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
@@ -157,7 +172,8 @@ def main():
         pretrained=True, progress=True)
     model.to(device)
     model.classifier = DeepLabHead(2048, nlabel)
-    criterion = nn.CrossEntropyLoss(weight=torch.tensor([1,1,20,20,20,60],dtype=torch.float32,device=device))
+    #criterion = nn.CrossEntropyLoss(weight=torch.tensor([1,1,20,20,20,60],dtype=torch.float32,device=device))
+    criterion = FocalLoss(alpha = torch.tensor([1,3,20,30,10,60],dtype=torch.float32,device=device))
     optimizer = torch.optim.SGD(model.parameters(),lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
     
     for epoch in range(args.resume_epoch, args.epochs):
